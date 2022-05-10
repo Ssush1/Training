f6f10779cda4e10478c098c58f629d5e39ed5bb0
Ssush1 <103404051+Ssush1@users.noreply.github.com>
2022-05-10 15:17:14 +0530

    g

--- project/Ecommerce1/webserver/src/Navigation.js
+++ project/Ecommerce1/webserver/src/Navigation.js
@@ -1,6 +1,22 @@
-import {};
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from "./Dashboard";
+import LoginPage from "./LoginPage";
+import SignupPage from "./SignupPage";
+import Productlist from "./Productlist";
 
-
-function  {
-  
-}
+function Navigation() {
+  return (
+    <div>
+        (Navigation)
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/SignupPage" element={<SignupPage />}></Route>
+          <Route path="/Dashboard" element={<SignupPage />}></Route>
+          <Route path="/Productlist" element={<SignupPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+};
+export default Navigation;
