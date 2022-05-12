a7556552dec7ed9a2a5fe2215269625bff87c633
Ssush1 <103404051+Ssush1@users.noreply.github.com>
2022-05-12 10:51:25 +0530

    12

--- project/Ecommerce1/webserver/src/Dashboard.js
+++ project/Ecommerce1/webserver/src/Dashboard.js
@@ -1,11 +1,28 @@
+
+import axios from "axios";
+import { ReactSession } from "react-client-session";
+import { useNavigate } from "react-router-dom";
 import './style.css'
-
 import { useEffect,useState } from 'react'
-import{}
-
 
 function Dashboard() {
+  const [sample, setSample]=useState('')
+  const [usr,setUsrName]=useState('')
+  const navigate=useNavigate();
+const [Productlist,setProductlist]=useState([ { id: 1, txtprodname: 'Sample1', Rate: '200', Tax: '15' },
+{ id: 2, txtprodname: 'Sample2', Rate: '800', Tax: '15' },
+{ id: 3, txtprodname: 'Sample3', Rate: '300', Tax: '15' },
+])
+//value hardcoded to test the navigation and data display
+
+useEffect(()=>{
+  if(ReactSession.get("username")==undefined)
+{navigate('/')}
+}
+
+setUsrName(ReactSession.get("user"))
   var a = [
+    -
     { id: 1, txtprodname: 'Sample1', Rate: '200', Tax: '15' },
     { id: 2, txtprodname: 'Sample2', Rate: '800', Tax: '15' },
     { id: 3, txtprodname: 'Sample3', Rate: '300', Tax: '15' },
@@ -17,4 +34,13 @@ function Dashboard() {
     { id: 3, txtprodname: 'Sample3', Rate: '300', Tax: '15' },
     
   ]);
+
+return(
+  <div>
+
+  </div>
+)
+
+
 }
+export default Dashboard;
